f5ff730aa75473d13d7ab3f94e5490669d07ff56
Bruno Mileto <brunomileto@outlook.com>
2021-05-23 18:40:18 -0300

    remove unused archives

--- client.dpr
+++ client.dpr
@@ -3,16 +3,12 @@ program client;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  uClient in 'uClient.pas' {FrmClient},
-  uc in 'uc.pas' {frmWSClient_FMX},
-  usv in 'usv.pas' {frmServer_FMX};
+  uClient in 'uClient.pas' {FrmClient};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TFrmClient, FrmClient);
-  Application.CreateForm(TfrmWSClient_FMX, frmWSClient_FMX);
-  Application.CreateForm(TfrmServer_FMX, frmServer_FMX);
   Application.Run;
 end.
